db323e190ae35ea00a1a21fdfa5f5a2b99045d80
Christian Sequeira <csequeira153@gmail.com>
2023-11-12 14:35:45 -0300

    fix:

--- src/repository/exerciseRepository.ts
+++ src/repository/exerciseRepository.ts
@@ -3,9 +3,21 @@ import prisma from "../config/prisma";
 
 export const findAll = async (bodyPart: BodyPart) => {
     if (bodyPart && Object.values(BodyPart).includes(bodyPart as BodyPart)) {
-        return await prisma.exercise.findMany({
-            where: { bodyPart },
+        return prisma.exercise.findMany({
+            where: {bodyPart},
         });
     }
-    return await prisma.exercise.findMany();
+    return prisma.exercise.findMany();
+};
+
+export const findById = async (id: string) => {
+    const exercise = await prisma.exercise.findUnique({
+        where: { id }
+    });
+
+    if (!exercise) {
+        throw new Error("exercise not found");
+    }
+
+    return exercise;
 };
