f5c2f68856b1094dffc975e81bec9db330e4ff4f
Christian Sequeira <csequeira153@gmail.com>
2024-06-06 16:40:41 -0300

    fix:

--- src/routes/exerciseRoute.ts
+++ src/routes/exerciseRoute.ts
@@ -1,7 +1,7 @@
-import { BodyPart } from "@prisma/client";
-import { FastifyInstance } from "fastify";
-import { ExerciseSchema } from "../schemas/exerciseSchema";
-import { findAllExercises } from "../services/exerciseService";
+import {BodyPart} from "@prisma/client";
+import {FastifyInstance} from "fastify";
+import {ExerciseSchema} from "../schemas/exerciseSchema";
+import {findAllExercises} from "../services/exerciseService";
 
 export default async function (fastify: FastifyInstance) {
     fastify.get<{ Querystring: { bodyPart: BodyPart } }>(
@@ -9,14 +9,12 @@ export default async function (fastify: FastifyInstance) {
         {
             schema: {
                 response: {
-                    200: { type: "array", ExerciseSchema },
+                    200: {type: "array", ExerciseSchema},
                 },
             },
         },
         async (req, rep) => {
-            console.log("DATABASE_URL: ", process.env.DATABASE_URL);
-
-            const { bodyPart } = req.query;
+            const {bodyPart} = req.query;
 
             const exercises = await findAllExercises(bodyPart);
             rep.status(200).send(exercises);
