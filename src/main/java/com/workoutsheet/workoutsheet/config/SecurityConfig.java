edf9866dd3770cc7b9dec1929bd978e14cb7c2ac
Christian Sequeira <csequeira153@gmail.com>
2025-03-26 16:17:38 -0300

    feat: add health check endpoint and update security configuration for public access

--- src/main/java/com/workoutsheet/workoutsheet/config/SecurityConfig.java
+++ src/main/java/com/workoutsheet/workoutsheet/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/clients").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/health").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/authenticate").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/authenticate").permitAll()
                         .anyRequest().authenticated()
                 )
--- src/main/java/com/workoutsheet/workoutsheet/controller/HealthController.java
+++ src/main/java/com/workoutsheet/workoutsheet/controller/HealthController.java
@@ -4,7 +4,7 @@ import com.workoutsheet.workoutsheet.facade.vm.workout.HealthResponseVM;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HealthController {
 
-    @PatchMapping("/")
+    @GetMapping
     public ResponseEntity<HealthResponseVM> healthChekc() {
         return ResponseEntity.ok(HealthResponseVM.builder().build());
     }
